de4871e4fccd934f90327dd409908402574d1430
30225885 <manavnarendrakumar.p@ucalgary.ca>
2025-06-23 15:02:10 -0600

    Final touches to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,17 +2,10 @@ import { ArrowUp } from "lucide-react";
 
 export const Footer = () => {
   return (
-    <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap jutify-between items-center">
-      <p className="text-sm text-muted-foreground justify-center ">
-        &copy; {new Date().getFullYear()} Manav Patel. All rights reserved.
+    <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-center items-center">
+      <p className="text-sm text-muted-foreground">
+        &copy; {new Date().getFullYear()} <a href="#hero" className="text-primary font-semibold">Manav Patel</a>. All rights reserved.
       </p>
-
-      <a
-        href="#hero"
-        className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors"
-      >
-        <ArrowUp size={20} />
-      </a>
     </footer>
   );
 };
